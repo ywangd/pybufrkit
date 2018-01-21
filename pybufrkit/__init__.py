eb8d428a1b9f53e64594d3b502bb79ce8ac5d075
Yang Wang <ywangd@gmail.com>
2018-01-22 09:13:17 +1100

    Add version 29 tables.
    Also made version 29 as default.
    Version bump.

--- pybufrkit/__init__.py
+++ pybufrkit/__init__.py
@@ -33,7 +33,7 @@ from pybufrkit.errors import (BitReadError,
                               UnknownDescriptor,
                               PyBufrKitError)
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 __author__ = 'ywangd@gmail.com'
 
 LOGGER = logging.getLogger('PyBufrKit')
