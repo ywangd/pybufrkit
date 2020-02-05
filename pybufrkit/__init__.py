8019c6e0b6fb831c7411ef5cffbe140b1a6384fb
Yang Wang <ywangd@gmail.com>
2020-02-05 20:02:51 +1100

    Bump version

--- pybufrkit/__init__.py
+++ pybufrkit/__init__.py
@@ -33,7 +33,7 @@ from pybufrkit.errors import (BitReadError,
                               UnknownDescriptor,
                               PyBufrKitError)
 
-__version__ = '0.2.13'
+__version__ = '0.2.14'
 __author__ = 'ywangd@gmail.com'
 
 LOGGER = logging.getLogger('PyBufrKit')
