d198d5d4efa2b7c47e722b1bb7e5f4ea61cebb1c
Yang Wang <ywangd@gmail.com>
2020-08-12 22:25:30 +1000

    Add WMO v34 BUFR tables and bump version

--- pybufrkit/__init__.py
+++ pybufrkit/__init__.py
@@ -33,7 +33,7 @@ from pybufrkit.errors import (BitReadError,
                               UnknownDescriptor,
                               PyBufrKitError)
 
-__version__ = '0.2.16'
+__version__ = '0.2.17'
 __author__ = 'ywangd@gmail.com'
 
 LOGGER = logging.getLogger('PyBufrKit')
