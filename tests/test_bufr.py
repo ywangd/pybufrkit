ywangd/pybufrkit

commit 495dc1d54f8bbae74ee6a4905d119dbac8b9bb71

Add support for optional parameter
BUFR spec allows an optional parameter at the end of section 1 for local
usage. This commit adds proper support for it.

Also bump version to 0.2.23

Resolves: #39

pybufrkit/bufr.py
@@ -27,7 +27,7 @@ class SectionParameter(object):
     This class represents a Parameter of a Bufr Section.
     """
 
-    def __init__(self, name, nbits, data_type, expected, as_property, value=None):
+    def __init__(self, name, nbits, data_type, expected, as_property, optional=False, value=None):
         # type: (str, int, str, object, bool) -> None
         self.name = name
         self.nbits = nbits
@@ -38,8 +38,12 @@ class SectionParameter(object):
             expected = expected.encode('utf-8')
         self.expected = expected
         self.as_property = as_property
+        self.optional = optional
         self.parent = None  # the section that parameter belongs to
         self.value = value
+
+    def __str__(self):
+        return 'name={}, type={}, nbits={}, value={}'.format(self.name, self.type, self.nbits, self.value)
 
 
 class SectionNamespace(OrderedDict):
@@ -126,6 +130,9 @@ class BufrSection(object):
         else:
             raise PyBufrKitError('Parameter "{}" not found'.format(parameter_name))
 
+    def has_optional_last_parameter(self):
+        return list(self._namespace.values())[-1].optional
+
 
 # An indicator for using the fallback default edition of a section
 DEFAULT_SECTION_EDITION = 0
@@ -183,18 +190,25 @@ class SectionConfigurer(object):
         section.set_metadata('optional', config.get('optional', False))
         section.set_metadata('end_of_message', config.get('end_of_message', False))
 
-        for parameter in config['parameters']:
+        for idx, parameter in enumerate(config['parameters']):
             data_type = parameter['type']
             nbits = parameter['nbits']
             if data_type == 'bytes':
                 assert nbits % NBITS_PER_BYTE == 0, \
                     'nbits for bytes type must be integer multiple of 8: {}'.format(nbits)
+            if nbits == 0:
+                assert data_type in ('bin', 'bytes', 'unexpanded_descriptors', 'template_data'), \
+                    'section {} parameter {} with nbits 0 has incorrect data type {}'.format(
+                                                                                          section_index, parameter['name'], data_type)
+                assert idx == len(config['parameters']) - 1, \
+                    'section {} parameter {} with nbits 0 must be at the end'.format(section_index, parameter['name'])
             section_parameter = SectionParameter(
                 parameter['name'],
                 nbits,
                 data_type,
                 parameter.get('expected', None),
-                parameter.get('as_property', False)
+                parameter.get('as_property', False),
+                parameter.get('optional', False)
             )
             section.add_parameter(section_parameter)
 
@@ -242,11 +256,15 @@ class SectionConfigurer(object):
         """
         section = self.configure_section(bufr_message, section_index)
         if section is not None:
-            assert len(section) == len(values), \
-                'Number of Section parameters ({}) not equal to number of values to be encoded ({})'.format(
+            if section.has_optional_last_parameter():
+                assert len(section) == len(values) or len(section) == len(values) + 1, \
+                    'Inconsistent number of section parameters {} and number of values {}'.format(len(section), len(values))
+            else:
+                assert len(section) == len(values), \
+                    'Number of Section parameters ({}) not equal to number of values to be encoded ({})'.format(
                     len(section), len(values))
-            for idx, parameter in enumerate(section):
-                parameter.value = values[idx] if overrides is None else overrides.get(parameter.name, values[idx])
+            for idx, (parameter, value) in enumerate(zip(section, values)):
+                parameter.value = value if overrides is None else overrides.get(parameter.name, value)
 
         return section
 

.github/workflows/ci.yml
@@ -22,6 +22,6 @@ jobs:
         python-version: ${{ matrix.python-version }}
     - run: pip install --upgrade pip
     - if: ${{ matrix.python-version }} == "3.11"
-      run: pip install ruff && ruff --output-format=github --ignore=E501 --target-version=py37 .
+      run: pip install ruff && ruff check --output-format=github --ignore=E501 --target-version=py37 .
     - run: pip install pytest .
     - run: pytest

pybufrkit/__init__.py
@@ -33,7 +33,7 @@ from pybufrkit.errors import (BitReadError,
                               UnknownDescriptor,
                               PyBufrKitError)
 
-__version__ = '0.2.22'
+__version__ = '0.2.23'
 __author__ = 'ywangd@gmail.com'
 
 LOGGER = logging.getLogger('PyBufrKit')

pybufrkit/encoder.py
@@ -147,7 +147,15 @@ class Encoder(Coder):
             elif parameter.type == PARAMETER_TYPE_TEMPLATE_DATA:
                 self.process_template_data(bufr_message, bit_writer, parameter)
             else:
-                bit_writer.write(parameter.value, parameter.type, parameter.nbits)
+                if parameter.nbits == 0:
+                    if parameter.value is None:
+                        continue
+                    if parameter.type == 'bytes':
+                        bit_writer.write_bytes(parameter.value, len(parameter.value))
+                    else:
+                        bit_writer.write(parameter.value, parameter.type, parameter.nbits)
+                else:
+                    bit_writer.write(parameter.value, parameter.type, parameter.nbits)
 
             log.debug('{} = {!r}'.format(parameter.name, parameter.value))
 

tests/test_optional_parameter.py
@@ -1,0 +1,60 @@
+from pybufrkit.decoder import Decoder
+from pybufrkit.encoder import Encoder
+
+# Special thanks to https://github.com/ReallyNiceGuy for identifying the bug and providing the test case.
+data = [
+    [
+        b'BUFR',
+        0,  # file length (will be calculated)
+        4
+    ],
+    [0,  # section length (will be calculated)
+     0,  # master table
+     0,  # centre
+     0,  # subcentre
+     0,  # sequence number
+     False,  # has section 2 (no)
+     '0000000',  # flag bits
+     6,  # data category
+     0,  # i18n subcategory
+     2,  # data local subcategory
+     11,  # master table version
+     10,  # local table version
+     0,  # year
+     0,  # month
+     0,  # day
+     0,  # hour
+     0,  # min
+     0,  # sec
+     b'\xff\xff'],  # Extra bytes
+    [0,  # section length (will be calculated)
+     '00000000',  # reserved bits
+     1,  # subsets
+     True,  # is observation
+     False,  # is compressed
+     '000000',  # flag bits
+     # Definition follows
+     []
+     ],
+    [
+        0,  # section length (will be calculated)
+        '00000000',  # flag bits
+        [
+            [
+            ]  # flat data
+        ]
+    ],
+    [b'7777']
+]
+
+
+def test_overriding_master_table_version():
+    encoder = Encoder()
+    bufr_message = encoder.process(data)
+    assert bufr_message.sections[1].section_length.value == 24
+    assert bufr_message.local_bytes.value == b'\xff\xff'
+
+    decoder = Decoder()
+    decoded = decoder.process(bufr_message.serialized_bytes)
+    assert decoded.sections[1].section_length.value == 24
+    assert decoded.local_bytes.value == b'\xff\xff'

commit 489dd974d281d7674e4957364e726225c1b00d7c

Fix default value for some section parameters
This is bug introduced in 495dc1d54f8bbae74ee6a4905d119dbac8b9bb71 which
makes master_table_number of originating_subcentre defaults to None
instead of 0. This commit fixes it.

Resolves: #42

pybufrkit/bufr.py
@@ -340,10 +340,10 @@ class BufrMessage(object):
         self._edition = None
         # Default to zero for edition 1 which does not have this parameter
         self._master_table_number = SectionParameter('master_table_number',
-                                                     0, 'unit', None, True, 0)
+                                                     0, 'unit', None, True, value=0)
         # Default to zero for edition 1 and 2 which do not have this parameter
         self._originating_subcentre = SectionParameter('originating_subcentre',
-                                                       0, 'unit', None, True, 0)
+                                                       0, 'unit', None, True, value=0)
 
     def add_section(self, section):
         """

tests/test_bufr.py
@@ -1,0 +1,7 @@
+from pybufrkit.bufr import BufrMessage
+
+
+def test_bufr_message():
+    bufr_message = BufrMessage()
+    assert bufr_message.master_table_number.value == 0
+    assert bufr_message.originating_subcentre.value == 0