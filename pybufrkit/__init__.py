47e500a88c040d81dbb8edff490d083de17de38b
Yang Wang <yang.wang@elastic.co>
2025-03-28 21:27:34 +1100

    Bump version to 0.2.25

--- pybufrkit/__init__.py
+++ pybufrkit/__init__.py
@@ -34,7 +34,7 @@ from pybufrkit.errors import (BitReadError,
                               UnknownDescriptor,
                               PyBufrKitError)
 
-__version__ = '0.2.24'
+__version__ = '0.2.25'
 __author__ = 'ywangd@gmail.com'
 
 LOGGER = logging.getLogger('PyBufrKit')
